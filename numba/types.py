4a6cf6eb47065907619b353264b1b87f41b91195
Graham Markall <graham.markall@continuum.io>
2014-12-29 15:30:09 +0000

    [WIP] Tidying

--- numba/types.py
+++ numba/types.py
@@ -541,17 +541,6 @@ class Void(Type):
         # types become supported in future.
         return False
 
-    @property
-    def size(self):
-        return len(self) * (self.dtype.bitwidth // 8)
-
-    @property
-    def ndim(self):
-        if isinstance(self.shape, tuple):
-            return len(self.shape)
-        else:
-            return 1
-
     def __len__(self):
         l = 1
         for s in self.shape:
