700dc3dac6e268f51b9013e0756ce766f5b2a09c
Graham Markall <graham.markall@continuum.io>
2015-11-06 12:55:57 +0000

    Add doc

--- examples/sobel.py
+++ examples/sobel.py
@@ -1,15 +1,31 @@
-from array import array
+# This demo is based on the PyPy Sobel demo, originally obtained from:
+#
+# https://bitbucket.org/pypy/extradoc/src/df759011beb46610c02d065ae159263817a2aa0b/talk/dls2012/benchmarks/image/?at=extradoc
+#
+# To run the demo with the provided video file, use:
+#
+# $ python sobel.py
+#
+# To run with an alternative file, arguments to mplayer can be specified on the
+# command line, e.g.:
+#
+# $ python sobel.py tv://
+#
+# to run in realtime from a webcam.
+
+from __future__ import print_function, division
 import numpy as np
+import os
+import re
+
 from math import sqrt
 from numba import jit
-
-import os, re, array
 from subprocess import Popen, PIPE, STDOUT
 
 
-def mplayer(Image, fn='tv://', options=''):
-    f = os.popen('mplayer -really-quiet -noframedrop ' + options + ' ' 
-                 '-vo yuv4mpeg:file=/dev/stdout 2>/dev/null </dev/null ' + fn)
+def mplayer(args):
+    f = os.popen('mplayer -really-quiet -noframedrop '
+                 '-vo yuv4mpeg:file=/dev/stdout 2>/dev/null </dev/null ' + args)
     hdr = f.readline()
     m = re.search('W(\d+) H(\d+)', hdr)
     w, h = int(m.group(1)), int(m.group(2))
@@ -17,12 +33,14 @@ def mplayer(Image, fn='tv://', options=''):
         hdr = f.readline()
         if hdr != 'FRAME\n':
             break
-        yield Image(w, h, fromfile=f)
-        f.read(w*h/2) # Color data
+        yield get_image(w, h, fromfile=f)
+        f.read(w*h//2) # Color data
+
 
 class MplayerViewer(object):
     def __init__(self):
         self.width = self.height = None
+
     def view(self, img):
         if not self.width:
             w, h = img.shape[0], img.shape[1]
@@ -31,23 +49,24 @@ class MplayerViewer(object):
                                  '-rawvideo', 'w=%d:h=%d:format=y8' % (w, h),
                                  '-really-quiet'],
                                  stdin=PIPE, stdout=PIPE, stderr=PIPE)
-            
             self.width = w
             self.height = h
         assert self.width == img.shape[0]
         assert self.height == img.shape[1]
-        img.T.tofile(self.mplayer.stdin)
+        write_image(img, self.mplayer.stdin)
 
 default_viewer = MplayerViewer()
 
 def view(img):
     default_viewer.view(img)
- 
+
+
 def get_image(w, h, fromfile):
     return np.fromfile(fromfile, dtype=np.uint8, count=w*h).reshape((h, w)).transpose()
 
 def write_image(img, f):
     img.T.tofile(f)
+
 
 @jit
 def sobel_magnitude(a):
@@ -63,19 +82,22 @@ def sobel_magnitude(a):
 
     return b
 
+
 if __name__ == '__main__':
     import sys
     from time import time
 
     if len(sys.argv) > 1:
-        fn = sys.argv[1]
+        mp_args = ' '.join(sys.argv[1:])
     else:
-        fn = 'sobel_test.avi -vf scale=640:480 -benchmark'
+        mp_args = 'sobel_test.avi -vf scale=640:480 -benchmark'
 
     start = start0 = time()
-    for fcnt, img in enumerate(mplayer(get_image, fn)):
+    for fcnt, img in enumerate(mplayer(mp_args)):
         view(sobel_magnitude(img))
-        print 1.0 / (time() - start), 'fps, ', (fcnt-2) / (time() - start0), 'average fps'
+        fps = 1.0 / (time() - start)
+        avg = (fcnt-2) / (time() - start0)
+        print('%s fps, %s average fps' % (fps, avg))
         start = time()
         if fcnt==2:
             start0 = time()
