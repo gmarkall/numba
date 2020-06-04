a3bf07739509a0e6a809fa0b39d207082398adc6
Graham Markall <gmarkall@nvidia.com>
2020-06-04 17:13:12 +0100

    Use enums from driver. use open_cudalib for lib

--- numba/cuda/cudart/enums.py
+++
@@ -1,1 +0,0 @@
-cudaSuccess = 0
--- numba/cuda/cudart/runtime.py
+++ numba/cuda/cudart/runtime.py
@@ -1,14 +1,12 @@
 import ctypes
 import functools
 import logging
-import os
 import sys
 
-from itertools import product
 from numba.core import config, utils
+from numba.cuda.cudadrv.libs import open_cudalib
 from numba.cuda.cudart.rtapi import API_PROTOTYPES
-from numba.cuda.cudart import enums
-from numba.cuda.envvars import get_numba_envvar
+from numba.cuda.cudadrv import enums
 from numba.cuda.errors import CudaSupportError, CudaRuntimeError
 
 
@@ -36,81 +34,11 @@ def _make_logger():
     return logger
 
 
-def find_runtime():
-
-    envpath = get_numba_envvar('CUDA_RUNTIME')
-
-    if envpath == '0':
-        # Force fail
-        _raise_runtime_not_found()
-
-    # Determine DLL type
-    if sys.platform == 'win32':
-        dlloader = ctypes.WinDLL
-        dldir = ['\\windows\\system32']
-        dlnames = ['cudart.dll']
-    else:
-        # Assume to be *nix like
-        dlloader = ctypes.CDLL
-        dldir = ['/usr/lib', '/usr/local/cuda/lib64'] # FIXME - path
-        dlnames = ['libcudart.so']
-
-    if envpath is not None:
-        try:
-            envpath = os.path.abspath(envpath)
-        except ValueError:
-            raise ValueError("NUMBA_CUDA_DRIVER %s is not a valid path" %
-                             envpath)
-        if not os.path.isfile(envpath):
-            raise ValueError("NUMBA_CUDA_DRIVER %s is not a valid file "
-                             "path.  Note it must be a filepath of the .so/"
-                             ".dll/.dylib or the driver" % envpath)
-        candidates = [envpath]
-    else:
-        # First search for the name in the default library path.
-        # If that is not found, try the specific path.
-        candidates = dlnames + [os.path.join(x, y)
-                                for x, y in product(dldir, dlnames)]
-
-    # Load the driver; Collect driver error information
-    path_not_exist = []
-    driver_load_error = []
-
-    for path in candidates:
-        try:
-            dll = dlloader(path)
-        except OSError as e:
-            # Problem opening the DLL
-            path_not_exist.append(not os.path.isfile(path))
-            driver_load_error.append(e)
-        else:
-            return dll
-
-    # Problem loading driver
-    if all(path_not_exist):
-        _raise_runtime_not_found()
-    else:
-        errmsg = '\n'.join(str(e) for e in driver_load_error)
-        _raise_driver_error(errmsg)
-
-
-RUNTIME_NOT_FOUND_MSG = """
-CUDA runtime library cannot be found.
-If you are sure that a CUDA runtime is installed,
-try setting environment variable NUMBA_CUDA_RUNTIME
-with the file path of the CUDA runtime shared library.
-"""
-
-RUNTIME_LOAD_ERROR_MSG = """
-Possible CUDA rtunime libraries are found but error occurred during load:
-%s
-"""
-
 MISSING_FUNCTION_ERRMSG = "runtime missing function: %s."
 
 
 def _build_reverse_error_map():
-    prefix = 'cuda'
+    prefix = 'CUDA_ERROR'
     map = utils.UniqueDict()
     for name in dir(enums):
         if name.startswith(prefix):
@@ -119,19 +47,7 @@ def _build_reverse_error_map():
     return map
 
 
-def _getpid():
-    return os.getpid()
-
-
 ERROR_MAP = _build_reverse_error_map()
-
-
-def _raise_runtime_not_found():
-    raise CudaSupportError(RUNTIME_NOT_FOUND_MSG)
-
-
-def _raise_driver_error(e):
-    raise CudaSupportError(RUNTIME_LOAD_ERROR_MSG % e)
 
 
 class CudaRuntimeAPIError(CudaRuntimeError):
@@ -157,7 +73,7 @@ class Runtime:
                        "in the environment, or because CUDA is unsupported on "
                        "32-bit systems.")
                 raise CudaSupportError(msg)
-            self.lib = find_runtime()
+            self.lib = open_cudalib('cudart')
             self.load_error = None
         except CudaSupportError as e:
             self.load_error = e
@@ -201,7 +117,7 @@ class Runtime:
         return safe_cuda_api_call
 
     def _check_error(self, fname, retcode):
-        if retcode != enums.cudaSuccess:
+        if retcode != enums.CUDA_SUCCESS:
             errname = ERROR_MAP.get(retcode, "cudaErrorUnknown")
             msg = "Call to %s results in %s" % (fname, errname)
             _logger.error(msg)
