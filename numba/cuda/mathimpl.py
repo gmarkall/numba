19085b62b3eb9c20bf450ba1e27aa0ccf5b26cd1
Graham Markall <graham@big-grey.co.uk>
2020-07-31 17:42:02 +0100

    Implement binaries through libdevice module

--- numba/cuda/mathimpl.py
+++ numba/cuda/mathimpl.py
@@ -3,16 +3,9 @@ from numba.core import typing
 from numba.core.imputils import Registry
 from numba.types import float32, float64
 from numba.cuda import libdevice
+
 registry = Registry()
 lower = registry.lower
-
-
-def impl_unary(key, ty, libfunc):
-    def lower_unary_impl(context, builder, sig, args):
-        libfunc_impl = context.get_function(libfunc, typing.signature(ty, ty))
-        return libfunc_impl(builder, args)
-
-    lower(key, ty)(lower_unary_impl)
 
 
 unarys = []
@@ -42,8 +35,40 @@ unarys += [('atanh', 'atanhf', math.atanh)]
 unarys += [('tan', 'tanf', math.tan)]
 unarys += [('tanh', 'tanhf', math.tanh)]
 
+binarys = []
+binarys += [('copysign', 'copysignf', math.copysign)]
+binarys += [('atan2', 'atan2f', math.atan2)]
+binarys += [('pow', 'powf', math.pow)]
+binarys += [('fmod', 'fmodf', math.fmod)]
+binarys += [('hypot', 'hypotf', math.hypot)]
+
+
+def impl_unary(key, ty, libfunc):
+    def lower_unary_impl(context, builder, sig, args):
+        libfunc_impl = context.get_function(libfunc, typing.signature(ty, ty))
+        return libfunc_impl(builder, args)
+
+    lower(key, ty)(lower_unary_impl)
+
+
+def impl_binary(key, ty, libfunc):
+    def lower_binary_impl(context, builder, sig, args):
+        libfunc_impl = context.get_function(libfunc,
+                                            typing.signature(ty, ty, ty))
+        return libfunc_impl(builder, args)
+
+    lower(key, ty, ty)(lower_binary_impl)
+
+
 for fname64, fname32, key in unarys:
     impl32 = getattr(libdevice, fname32)
     impl64 = getattr(libdevice, fname64)
     impl_unary(key, float32, impl32)
     impl_unary(key, float64, impl64)
+
+
+for fname64, fname32, key in binarys:
+    impl32 = getattr(libdevice, fname32)
+    impl64 = getattr(libdevice, fname64)
+    impl_binary(key, float32, impl32)
+    impl_binary(key, float64, impl64)
