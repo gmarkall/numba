40f9b62932e7f09dfeb4365eec5eeb4516692676
Graham Markall <graham@big-grey.co.uk>
2020-08-03 11:59:09 +0100

    Undo Friday night's confused attempts

--- numba/cuda/libdevicedecl.py
+++ numba/cuda/libdevicedecl.py
@@ -1,31 +1,21 @@
 from numba.cuda import libdevice, libdevicefuncs
 
-from numba.core import types
 from numba.core.typing.templates import ConcreteTemplate, Registry, signature
 
 registry = Registry()
 register_global = registry.register_global
 
 
-def libdevice_declare(func, retty, argtys):
+def libdevice_declare(func, retty, args):
     class Libdevice_function(ConcreteTemplate):
-        cases = [signature(retty, *argtys)]
+        cases = [signature(retty, *[arg.ty for arg in args if not arg.is_ptr])]
 
     pyfunc = getattr(libdevice, func[5:])
     register_global(pyfunc)(Libdevice_function)
 
 
 for func, (retty, args) in libdevicefuncs.functions.items():
-    extra_return_argtys = [arg.ty for arg in args if arg.is_ptr]
-    # We strip off the pointer arguments from the signature, because they
-    # will be return values in CUDA Python.
-    argtys = [arg.ty for arg in args if not arg.is_ptr]
-    if extra_return_argtys:
-        if retty != types.void:
-            return_argtys = [retty] + extra_return_argtys
-        else:
-            return_argtys = extra_return_argtys
-        print(f'{func}, {return_argtys}')
-        retty = types.Tuple(return_argtys)
-
-    libdevice_declare(func, retty, argtys)
+    if any([arg.is_ptr for arg in args]):
+        print(f'Skipping {func} with pointer arg')
+        continue
+    libdevice_declare(func, retty, args)
--- numba/cuda/libdeviceimpl.py
+++ numba/cuda/libdeviceimpl.py
@@ -1,5 +1,4 @@
 from llvmlite.llvmpy.core import Type
-from numba.core import types
 from numba.core.imputils import Registry
 from numba.cuda import libdevice, libdevicefuncs
 
@@ -23,37 +22,9 @@ def libdevice_implement(func, retty, nbargs):
     lower(key, *argtys)(core)
 
 
-def libdevice_implement_tuple_return(func, retty, return_args, nb_retty,
-                                     nbargs):
-    def core(context, builder, sig, args):
-        #from pudb import set_trace; set_trace()
-        #lmod = builder.module
-        #fretty = context.get_value_type(retty)
-        fargtys = []
-        for arg in nbargs:
-            ty = context.get_value_type(arg.ty)
-            if arg.is_ptr:
-                ty = ty.as_pointer()
-            fargtys.append
-        print(f'{func}, {fargtys}')
-
-    key = getattr(libdevice, func[5:])
-
-    argtys = [arg.ty for arg in args if not arg.is_ptr]
-    lower(key, *argtys)(core)
-
-
 for func, (retty, args) in libdevicefuncs.functions.items():
-    extra_return_argtys = [arg.ty for arg in args if arg.is_ptr]
-    # __nv_nan[f] are weird
-    if extra_return_argtys and func not in ('__nv_nan', '__nv_nanf'):
-        if retty != types.void:
-            return_argtys = [retty] + extra_return_argtys
-        else:
-            return_argtys = extra_return_argtys
-        print(f'{func}, {return_argtys}')
-        nb_retty = types.Tuple(return_argtys)
-        libdevice_implement_tuple_return(func, retty, return_argtys, nb_retty,
-                                         args)
+    if any([arg.is_ptr for arg in args]):
+        print(f'Skipping {func} with pointer arg')
+        continue
 
     libdevice_implement(func, retty, args)
