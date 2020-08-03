f73a7c07b4985f97a219335cea43f38de11d0411
Graham Markall <graham@big-grey.co.uk>
2020-08-03 14:46:41 +0100

    Add typing for functions with multiple returns

--- numba/cuda/libdevicedecl.py
+++ numba/cuda/libdevicedecl.py
@@ -1,5 +1,5 @@
 from numba.cuda import libdevice, libdevicefuncs
-
+from numba.core import types
 from numba.core.typing.templates import ConcreteTemplate, Registry, signature
 
 registry = Registry()
@@ -15,12 +15,21 @@ def libdevice_declare(func, retty, args):
 
 
 def libdevice_declare_multiple_returns(func, retty, args):
-    #class Libdevice_function(ConcreteTemplate):
-    #    cases = [signature(retty, *[arg.ty for arg in args if not arg.is_ptr])]
+    # Any pointer arguments should be part of the return type.
+    return_types = [arg.ty for arg in args if arg.is_ptr]
+    # If the return type is void, there is no point adding it to the list of
+    # return types.
+    if retty != types.void:
+        return_types.insert(0, retty)
 
-    #pyfunc = getattr(libdevice, func[5:])
-    #register_global(pyfunc)(Libdevice_function)
-    print(f"Skipping typing {func} with pointer arg")
+    retty = types.Tuple(return_types)
+    argtypes = [arg.ty for arg in args if not arg.is_ptr]
+
+    class Libdevice_function(ConcreteTemplate):
+        cases = [signature(retty, *argtypes)]
+
+    pyfunc = getattr(libdevice, func[5:])
+    register_global(pyfunc)(Libdevice_function)
 
 
 for func, (retty, args) in libdevicefuncs.functions.items():
