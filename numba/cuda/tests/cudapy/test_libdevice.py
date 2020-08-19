e143de2f99353e9de8fe1075368463f7610fb296
Graham Markall <graham@big-grey.co.uk>
2020-08-19 16:10:13 +0100

    Add some more tests

--- numba/cuda/tests/cudapy/test_libdevice.py
+++ numba/cuda/tests/cudapy/test_libdevice.py
@@ -15,11 +15,27 @@ def use_sincos(s, c, x):
         c[i] = cr
 
 
+def use_frexp(frac, exp, x):
+    i = cuda.grid(1)
+
+    if i < len(x):
+        fracr, expr = libdevice.frexp(x[i])
+        frac[i] = fracr
+        exp[i] = expr
+
+
+def use_sad(r, x, y, z):
+    i = cuda.grid(1)
+
+    if i < len(x):
+        r[i] = libdevice.sad(x[i], y[i], z[i])
+
+
 class TestLibdevice(CUDATestCase):
     """
     Some tests of libdevice function wrappers that check the returned values.
 
-    These are mainly to check that the generation of all the implementations
+    These are mainly to check that the generation of the implementations
     results in correct typing and lowering for each type of function return
     (e.g. scalar return, UniTuple return, Tuple return, etc.).
     """
@@ -35,6 +51,32 @@ class TestLibdevice(CUDATestCase):
 
         np.testing.assert_allclose(np.cos(arr), cres)
         np.testing.assert_allclose(np.sin(arr), sres)
+
+    def test_frexp(self):
+        # Test return of a Tuple from a libdevice function
+        arr = np.linspace(start=1.0, stop=10.0, num=100, dtype=np.float64)
+        fracres = np.zeros_like(arr)
+        expres = np.zeros(shape=arr.shape, dtype=np.int32)
+
+        cufunc = cuda.jit(use_frexp)
+        cufunc[4, 32](fracres, expres, arr)
+
+        frac_expect, exp_expect = np.frexp(arr)
+
+        np.testing.assert_allclose(frac_expect, fracres)
+        np.testing.assert_allclose(exp_expect, expres)
+
+    def test_sad(self):
+        # Test return of a scalar from a libdevice function
+        x = np.arange(0, 200, 2)
+        y = np.arange(50, 150)
+        z = np.arange(15, 115)
+        r = np.zeros_like(x)
+
+        cufunc = cuda.jit(use_sad)
+        cufunc[4, 32](r, x, y, z)
+
+        np.testing.assert_allclose(np.abs(x - y) + z, r)
 
 
 # A template for generating tests of compiling calls to libdevice functions.
