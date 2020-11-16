0761a35ad77a13db87b31a9f3046f125cd3740d9
Graham Markall <gmarkall@nvidia.com>
2020-11-16 14:13:50 +0000

    Add ycm conf to help clangd

---
+++ .ycm_extra_conf.py
@@ -0,0 +1,9 @@
+flags = [
+    '-Inumba',
+    '-I/home/gmarkall/miniconda3/envs/numba/include/python3.8',
+    '-I/home/gmarkall/miniconda3/envs/numbatorch/include'
+]
+
+
+def Settings(**kwargs):
+    return { 'flags': flags }
