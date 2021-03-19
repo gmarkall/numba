8a9a2e65e7ab770974c1adc2b75aa928165fc0cd
Graham Markall <gmarkall@nvidia.com>
2021-03-19 12:57:27 +0000

    Fix test for nvvm 3.4

--- numba/cuda/tests/cudapy/test_debuginfo.py
+++ numba/cuda/tests/cudapy/test_debuginfo.py
@@ -1,6 +1,7 @@
 from numba.tests.support import override_config
 from numba.cuda.testing import skip_on_cudasim
 from numba import cuda
+from numba.cuda.cudadrv.nvvm import NVVM
 from numba.core import types
 from numba.cuda.testing import CUDATestCase
 import re
@@ -69,14 +70,31 @@ class TestCudaDebugInfo(CUDATestCase):
             x[0] = 1
 
         llvm_ir = f.inspect_llvm(sig)
-        defines = [line for line in llvm_ir.splitlines()
-                   if 'define void @"_ZN6cudapy' in line]
 
-        # Make sure we only found one definition
-        self.assertEqual(len(defines), 1)
+        if NVVM().is_nvvm70:
+            # NNVM 7.0 IR attaches a debug metadata reference to the
+            # definition
+            defines = [line for line in llvm_ir.splitlines()
+                       if 'define void @"_ZN6cudapy' in line]
 
-        wrapper_define = defines[0]
-        self.assertIn('noinline!dbg', wrapper_define)
+            # Make sure we only found one definition
+            self.assertEqual(len(defines), 1)
+
+            wrapper_define = defines[0]
+            self.assertIn('noinline!dbg', wrapper_define)
+        else:
+            # NVVM 3.4 subprogram debuginfo refers to the definition.
+            # '786478' is a constant referring to a subprogram.
+            disubprograms = [line for line in llvm_ir.splitlines()
+                             if '786478' in line]
+
+            # Make sure we only found one subprogram
+            self.assertEqual(len(disubprograms), 1)
+
+            wrapper_disubprogram = disubprograms[0]
+            # Check that the subprogram points to a wrapper (these are all in
+            # the "cudapy::" namespace).
+            self.assertIn('_ZN6cudapy', wrapper_disubprogram)
 
     def test_debug_function_calls_internal_impl(self):
         # Calling a function in a module generated from an implementation
