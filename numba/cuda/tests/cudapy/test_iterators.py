34d00f98b99891703a95bbd7690b212fb8f7676c
Graham Markall <gmarkall@nvidia.com>
2021-06-25 17:01:21 +0100

    WIP commit debugging segfault
    This branch contains some work debugging an NVVM segfault caused by
    Numba's LLVM IR.

--- numba/cuda/tests/cudapy/test_iterators.py
+++ numba/cuda/tests/cudapy/test_iterators.py
@@ -1,53 +1,98 @@
-from numba import cuda, njit
+from numba import cuda
 from numba.cuda.testing import unittest, CUDATestCase
 
 import numpy as np
 
+
 class TestIterators(CUDATestCase):
 
-    @unittest.skip
     def test_enumerate(self):
-        @cuda.jit#(debug=True)
-        def enumerator(x):
+        @cuda.jit
+        def enumerator(x, error):
             count = 0
 
             for i, v in enumerate(x):
                 if count != i:
-                    raise RuntimeError("Count and enumeration are unequal")
+                    error[0] = 1
                 if v != x[i]:
-                    raise RuntimeError("Value and enumeration are unequal")
+                    error[0] = 2
 
                 count += 1
 
             if count != len(x):
-                raise RuntimeError("Count did not equal length of x")
+                error[0] = 3
 
-        enumerator[1, 1]((10, 9, 8, 7, 6))
+        x = np.asarray((10, 9, 8, 7, 6))
+        error = np.zeros(1, dtype=np.int32)
+
+        enumerator[1, 1](x, error)
+        self.assertEqual(error[0], 0)
+
+    def _test_twoarg_function(self, f):
+        x = np.asarray((10, 9, 8, 7, 6))
+        y = np.asarray((1, 2, 3, 4, 5))
+        error = np.zeros(1, dtype=np.int32)
+
+        f[1, 1](x, y, error)
+        self.assertEqual(error[0], 0)
 
     def test_zip(self):
-        @cuda.jit(debug=True)
-        def zipper(x, y):
+        @cuda.jit
+        def zipper(x, y, error):
             i = 0
 
             for xv, yv in zip(x, y):
                 if xv != x[i]:
-                    pass #raise RuntimeError("xv and x[i] are unequal")
-                #else:
-                #    pass
-                #if yv != y[i]:
-                #    raise RuntimeError("yv and y[i] are unequal")
+                    error[0] = 1
+                if yv != y[i]:
+                    error[0] = 2
 
                 i += 1
 
-            #if i != len(x):
-            #    print("bad 3")
-            #    #raise RuntimeError("Count did not equal length of x")
+            if i != len(x):
+                error[0] = 3
 
-        x = np.asarray((10, 9, 8, 7, 6))
-        y = np.asarray((1, 2, 3, 4, 5))
-        #zipper(x, y)
-        zipper[1, 1](x, y)
-        #zipper[1, 1]((10, 9, 8, 7, 6), (1, 2, 3, 4, 5))
+        self._test_twoarg_function(zipper)
+
+    def test_enumerate_zip(self):
+        @cuda.jit
+        def enumerator_zipper(x, y, error):
+            count = 0
+
+            for i, (xv, yv) in enumerate(zip(x, y)):
+                if i != count:
+                    error[0] = 1
+                if xv != x[i]:
+                    error[0] = 2
+                if yv != y[i]:
+                    error[0] = 3
+
+                count += 1
+
+            if count != len(x):
+                error[0] = 4
+
+        self._test_twoarg_function(enumerator_zipper)
+
+    def test_zip_enumerate(self):
+        @cuda.jit
+        def zipper_enumerator(x, y, error):
+            count = 0
+
+            for (i, xv), yv in zip(enumerate(x), y):
+                if i != count:
+                    error[0] = 1
+                if xv != x[i]:
+                    error[0] = 2
+                if yv != y[i]:
+                    error[0] = 3
+
+                count += 1
+
+            if count != len(x):
+                error[0] = 4
+
+        self._test_twoarg_function(zipper_enumerator)
 
 
 if __name__ == '__main__':
