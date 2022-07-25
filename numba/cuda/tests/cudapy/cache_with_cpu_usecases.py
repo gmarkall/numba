0c9e8a78d117f167c39dd00115bde03e84f16a3a
Graham Markall <gmarkall@nvidia.com>
2022-07-25 10:51:13 +0100

    CUDA tests: Move use cases into their own module
    These UseCase classes will be useful for more than just cache usecases,
    so it helps to have them in their own module.

--- numba/cuda/tests/cudapy/cache_with_cpu_usecases.py
+++ numba/cuda/tests/cudapy/cache_with_cpu_usecases.py
@@ -2,12 +2,7 @@ import sys
 
 from numba import cuda, njit
 from numba.cuda.testing import CUDATestCase
-from numba.cuda.tests.cudapy.cache_usecases import CUDAUseCase, UseCase
-
-
-class CPUUseCase(UseCase):
-    def _call(self, ret, *args):
-        self._func(ret, *args)
+from numba.cuda.tests.cudapy.usecases import CPUUseCase, CUDAUseCase
 
 
 # Using the same function as a cached CPU and CUDA-jitted function
