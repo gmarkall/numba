dab1ed68d7a14146d570a128fbf746a131a9a408
Graham Markall <gmarkall@nvidia.com>
2022-11-11 17:29:53 +0000

    Pass some options to NVPTX backend, link in libdevice

--- numba/cuda/nvptx.py
+++ numba/cuda/nvptx.py
@@ -1,15 +1,27 @@
 from llvmlite import binding as ll
+from numba.cuda.cudadrv.nvvm import LibDevice
 
 ll.initialize_all_targets()
 ll.initialize_all_asmprinters()
 
 
 def llvm_to_ptx(irs, options):
+    #print(options)
+    opt = options['opt']
+    cpu = options['arch']
+    cpu = cpu.replace('compute_', 'sm_')
+
     target = ll.Target.from_triple('nvptx64-nvidia-cuda')
-    tm = target.create_target_machine()
+    tm = target.create_target_machine(opt=opt, cpu=cpu)
+    #breakpoint()
 
     mods = [ll.parse_assembly(ir) for ir in irs]
     main_module, *other_modules = mods
+
+    # XXX: Need to run the nvvm_reflect pass to eliminate unused libdevice
+    # functions.
+    libdevice = ll.parse_bitcode(LibDevice().get())
+    main_module.link_in(libdevice)
 
     for other in other_modules:
         main_module.link_in(other)
