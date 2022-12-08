3fc5c4da5023179b04f41dea1d5f2da4cddc6a3f
Graham Markall <gmarkall@nvidia.com>
2022-12-08 13:10:37 +0000

    Enough to get as far as needing to get global value address

---
+++ simple.py
@@ -0,0 +1,13 @@
+import os
+
+os.environ['NUMBA_TRACE'] = '1'
+os.environ['NUMBA_LLVM_REFPRUNE_PASS'] = '0'
+os.environ['NUMBA_OPT'] = '0'
+
+from numba import njit
+
+@njit(_nrt=False)
+def f(a, b):
+    return a + b
+
+print(f(1, 2))
