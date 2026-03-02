3c0c0c47e5e4c45269dac7c36fe235bb842e6925
Graham Markall <gmarkall@nvidia.com>
2026-03-02 12:09:58 +0000

    Hardcode NumPy type promotion rules

--- numba/core/types/abstract.py
+++ numba/core/types/abstract.py
@@ -251,17 +251,8 @@ class Number(Hashable):
     """
 
     def unify(self, typingctx, other):
-        """
-        Unify the two number types using Numpy's rules.
-        """
-        from numba.np import numpy_support
-        if isinstance(other, Number):
-            # XXX: this can produce unsafe conversions,
-            # e.g. would unify {int64, uint64} to float64
-            a = numpy_support.as_dtype(self)
-            b = numpy_support.as_dtype(other)
-            sel = np.promote_types(a, b)
-            return numpy_support.from_dtype(sel)
+        from numba.core.types.promote_types import PROMOTE_TYPES
+        return PROMOTE_TYPES.get((self, other), None)
 
 
 class Callable(Type):
